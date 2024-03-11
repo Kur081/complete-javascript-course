fa8eddf453b1ea2036fece33f18ed3e9953642a1
Matthew Cameron <matthew.cameron2@students.mq.edu.au>
2024-03-11 18:30:22 +1100

    Completed practicals of Javascript course

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,8 +1,35 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+//Values and Variables
+//Data Types
+//let, const and var
+//Basic operators
+const country = 'Australia';
+const continent = 'Australia';
+let population = 27;
+const isIsland = false;
+const language = 'English';
 
-console.log("Jonas");
-console.log(23);
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(isIsland);
+console.log(language);
 
-let firstName = "Bob";
-console.log(firstName);
+console.log(population / 2);
+
+population++;
+console.log(population);
+
+console.log(population > 6);
+
+console.log(population < 33);
+
+const description =
+    country +
+    ' is in ' +
+    continent +
+    ', and its ' +
+    population +
+    ' million people speak ' +
+    language;
+
+console.log(description);
