edc3ad6546c2a58d8cbfe29888cc706e7741bbb2
Matthew Cameron <matthew.cameron2@students.mq.edu.au>
2024-03-16 15:11:14 +1100

    progressed through Assignments in js course

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,6 +3,12 @@
 //let, const and var
 //Basic operators
 //Strings and Template Literals
+//Taking Decisions: If/Else statements
+//Type conversion and coercion
+//Equality Operators: == vs ===
+//Logical Operators
+//Switch
+
 const country = 'Australia';
 const continent = 'Australia';
 let population = 27;
@@ -34,7 +40,7 @@ if (population > 33) {
     console.log(`${country}'s population is below average`)
 };
 
-
+/*
 //4
 //23
 //617
@@ -46,3 +52,45 @@ console.log('19' - '13' + '17'); // -> 23
 console.log('19' - '13' + 17); // -> 617
 console.log('123' < 57); // -> false
 console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143
+
+
+const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+
+if (numNeighbours === 1) {
+    console.log("Only 1 border!");
+} else if (numNeighbours > 1) {
+    console.log('More than 1 border');
+} else {
+    console.log('No borders')
+}
+
+*/
+
+let shouldLive = language === 'English' && population < 50 && !isIsland;
+
+if (shouldLive) {
+    console.log('You should live in Australia :)');
+} else {
+    console.log('Australia does not meet your criteria :(');
+}
+
+switch (language) {
+    case 'chinese':
+    case 'mandarin':
+        console.log('MOST number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'English':
+        console.log('3rd place');
+        break;
+    case 'hindi':
+        console.log('Number 4');
+        break;
+    case 'arabic':
+        console.log('5th most spoken language');
+        break;
+    default:
+        console.log('Great language too :D');
+}
